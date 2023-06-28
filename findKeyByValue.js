0739bdb4b2dad1d1e7ccf6eec588e362570b01f1
vagrant <hassan.a.issak@gmail.com>
2023-06-28 22:40:31 +0000

    Created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const findKeyByValue = function(input, value) {
+  let output;
+  for (let key in input) {
+    if (input[key] === value) {
+      return output = key;
+    }
+  }
+  return output;
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
