e4f8b11935f54613cd84e3fe43154b9b32edd432
vagrant <hassan.a.issak@gmail.com>
2023-07-03 19:39:24 +0000

    created headTest.js

--- head.js
+++ head.js
@@ -6,13 +6,6 @@ const assertEqual = require('./assertEqual');
 
 
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-//   }
-// };
+module.exports = head
 
-assertEqual(head([5,6,7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
