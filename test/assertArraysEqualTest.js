11e9f154294d2c3e73a612fd5f380bc7ac4fa8ef
vagrant <hassan.a.issak@gmail.com>
2023-07-03 23:16:31 +0000

    Cleaned up assertArraysEqual

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,9 +2,7 @@ const eqArrays = require('../eqArrays')
 const assertArraysEqual = require('../assertArraysEqual');
 const assert = require('chai').assert;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
 
 
 describe("asserArraysEqual", () => {
