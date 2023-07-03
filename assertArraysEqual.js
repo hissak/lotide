a816c7dfd5c22d2c15ab8a9831d61443dfa860da
vagrant <hassan.a.issak@gmail.com>
2023-07-03 19:27:10 +0000

    assertArraysEqual refactored to import functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,7 @@
+const eqArrays = require('./eqArrays');
+
+
+
 const assertArraysEqual = function(arrayA, arrayB) {
   if (eqArrays(arrayA, arrayB)) {
     console.log(`✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}`);
@@ -6,33 +10,27 @@ const assertArraysEqual = function(arrayA, arrayB) {
   }
 };
 
+module.exports = assertArraysEqual;
+// const eqArrays = function(array1, array2) {
+//   let newArray = [];
+//   let truthyFalsey = true;
+//   if (array1.length === array2.length) {
+//     for (let i = 0; i < array1.length && i < array2.length; i++) {
+//       if (array1[i] === array2[i]) {
+//         newArray.push(true);
+//       } else {
+//         newArray.push(false);
+//       }
+//     }
+//   } else if (array1.length !== array2.length) {
+//     return truthyFalsey = false;
+//   }
 
-const eqArrays = function(array1, array2) {
-  let newArray = [];
-  let truthyFalsey = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length && i < array2.length; i++) {
-      if (array1[i] === array2[i]) {
-        newArray.push(true);
-      } else {
-        newArray.push(false);
-      }
-    }
-  } else if (array1.length !== array2.length) {
-    return truthyFalsey = false;
-  }
+//   for (let x = 0; x < newArray.length; x++) {
+//     if (!newArray[x]) {
+//       return truthyFalsey = false;
+//     }
+//   }
+//   return truthyFalsey;
+// };
 
-  for (let x = 0; x < newArray.length; x++) {
-    if (!newArray[x]) {
-      return truthyFalsey = false;
-    }
-  }
-  return truthyFalsey;
-};
-
-module.exports = eqArrays
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
